617ac6f4d29299d4df0aadf25105a8a7b4e89002
Vaughan SCOTT <vscott@c5r3s3.wethinkcode.co.za>
2019-11-13 17:42:08 +0200

    can canvas uploaded image now to save canvas.

--- cam.js
+++ cam.js
@@ -5,15 +5,22 @@
         context = canvas.getContext('2d');
 
     navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    navigator.getMedia({
-       video: true,
-       audio: false
-    }, function(stream) {
-       video.srcObject = stream;
-       video.play();
-    }, function(error) {
-    });
+    upload = new Image();
+    upload.src = "./tmp/tmp.png";
+    upload.onload = function(){
+        context.drawImage(upload, 0,0, 500, 375);
+        document.getElementById("canvas").style.zIndex = "2";
+        navigator.getMedia({
+           video: true,
+           audio: false
+        }, function(stream) {
+           video.srcObject = stream;
+           video.play();
+        }, function(error) {
+        });
+    }
+    
+    
     document.getElementById('capture').addEventListener('click', function() {
     context.drawImage(video, 0, 0, 500, 375);
     document.getElementById("canvas").style.zIndex = "1";
@@ -24,3 +31,14 @@ function camReset() {
     document.getElementById("canvas").style.zIndex = "-1";
  }
 
+// function  upload() {
+//     var canvas2 = document.getElementById('canvas2'),
+//         context = canvas2.getContext('2d');
+//     upload = new Image();
+//     upload.src = "./tmp/tmp.png";
+//     context.drawImage(upload, 0,0, 500, 375);
+//         document.getElementById("canvas2").style.zIndex = "1";
+//     // context = canvas.getContext('2d');
+//     // context.drawImage(upload, 0, 0, 500, 375);
+//     // document.getElementById("canvas").style.zIndex = "99";
+// }
--- cam.php
+++ cam.php
@@ -3,44 +3,46 @@ require_once 'core/init.php';
 
 $user = new User();
 
-// if(!$user->isLoggedIn()) {
-//     Redirect::to('index.php');
-// }
+if(!$user->isLoggedIn()) {
+    Redirect::to('index.php');
+}
 
 if (isset($_POST['submit'])) {
     $file = $_FILES['file'];
 
     $fileName = $file['name'];
     $fileTmpName = $file['tmp_name'];
-    $src_size = getimagesize($fileTmpName);
-    // echo $src_size[" height="];
-    list($width, $height) = getimagesize($fileTmpName);
+    if ($fileTmpName)
+    {
+        $src_size = getimagesize($fileTmpName);
+        // echo $src_size[" height="];
+        list($width, $height) = getimagesize($fileTmpName);
 
 
 
-    $tmp = imagecreatetruecolor(500, 375);
+        $tmp = imagecreatetruecolor(500, 375);
 
-    $tmp_path = "./tmp/tmp.png";
+        $tmp_path = "./tmp/tmp.png";
 
-    $fileExt = explode('.', $fileName);
-    $fileActualExt = strtolower(end($fileExt));
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
 
-    switch($fileActualExt) {
-        case "jpg":
-            $source = imagecreatefromjpeg($fileTmpName);
-            break;
-        case "jpeg":
-            $source = imagecreatefromjpeg($fileTmpName);
-            break;
-        case "png":
-            $source = imagecreatefrompng($fileTmpName);
-            break;
+        switch($fileActualExt) {
+            case "jpg":
+                $source = imagecreatefromjpeg($fileTmpName);
+                break;
+            case "jpeg":
+                $source = imagecreatefromjpeg($fileTmpName);
+                break;
+            case "png":
+                $source = imagecreatefrompng($fileTmpName);
+                break;
+        }
+        imagecopyresampled($tmp , $source , 0, 0, 0 , 0, 
+        500 , 375 , $width , $height );
+
+        imagepng($tmp, $tmp_path, 9);
     }
-    imagecopyresampled($tmp , $source , 0, 0, 0 , 0, 
-    500 , 375 , $width , $height );
-
-    imagepng($tmp, $tmp_path, 9);
-
     ?>
         
     <?php
@@ -52,7 +54,7 @@ if (isset($_POST['submit'])) {
     </head>
     <body>
     <div class="booth">
-    <img id="upload" src="./tmp/tmp.png" alt="">
+    <!-- <img id="upload" src="./tmp/tmp.png" alt=""> -->
     <!-- <img id="stick" style="position:absolute; width: 90%;"> -->
     <video id="video" width="100%"></video>
     <canvas id="canvas" width="500" height="380" ></canvas>
@@ -60,15 +62,15 @@ if (isset($_POST['submit'])) {
         <a href ="#" id="capture" class="capturebtn">Take Photo</a>
         <!-- <canvas id="sticker" width="400" height="300" style="position: absolute;top: 0px;left: 0px;z-index: 2;width: 100%;"></canvas> -->
         
-        <button type="button" style="font-size: 1.6vh;" class="uploadbtn" onclick="camReset()">Retake photo</button>
+        <button type="button" style="font-size: 1.6vh;" class="" onclick="camReset()">Retake photo</button>
         <form action="" method="POST" enctype="multipart/form-data">
         <input type="file" name="file">
-        <button type="submit" name="submit" onclick="upload()">UPLOAD</button>
+        <button type="submit" name="submit" required="">UPLOAD</button>
         </form>
+        
         <!-- <a href ="#" id="upload" class="uploadbtn">Upload</a> -->
     </div>
     </div>
     </body>
     <script src="cam.js"></script>
 </html>
-
